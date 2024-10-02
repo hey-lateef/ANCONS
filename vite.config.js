4a282b0071f803f7087b95adb89229e31df65101
Syed Rahman Ur Lateef <rahmanlateef5@gmail.com>
2024-10-02 22:43:44 +0530

    Created App

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: "ANCONS",
+  
   plugins: [react()],
 })
